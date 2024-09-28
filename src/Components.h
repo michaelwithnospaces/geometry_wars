34a84c4493cd0b55e4fae2f80e7c136eac64700b
Michael <michaelwithnospaces@proton.me>
2024-09-28 18:00:12 -0500

    feat: Shape component added

--- src/Components.h
+++ src/Components.h
@@ -2,6 +2,7 @@
 #define COMPONENTS_H
 
 #include "raylib.h"
+#include "Vec2.h"
 
     class CTransform 
     {
@@ -10,7 +11,14 @@
 
     class CShape
     {
+    public:
+        Vec2f center;
+        float r;
+        int sides;
+        Color color;
 
+        CShape() : center({0.0f, 0.0f}), sides(3), r(1.0f), color({255, 255, 255, 255}) {};
+        CShape(Vec2f _center, int _sides, float _r, Color _color) : center(_center), sides(_sides), r(_r), color(_color) {};
     };
 
     class CCollision
